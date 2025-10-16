ad5a48efff4e87068bfa55ce5c00b08f72b40a52
Caliodore <caliodorecharbonneau@gmail.com>
2025-10-15 19:36:55 -0500

    Basic Player Movement On First Map
    Added a basic player capsule with movement on a basic layout of one of the maps I've planned. In Scenes/2ndBankAttempt is the current one I was working on.

--- Team4_StealthGame/Assets/Scripts/Basic Testing/PlayerMovement.cs
+++ Team4_StealthGame/Assets/Scripts/Basic Testing/PlayerMovement.cs
@@ -1,16 +1,43 @@
+using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Interactions;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [SerializeField] float playerWalkSpeed, playerRunSpeed, playerSneakSpeed, currentPlayerSpeed;
+
+    [SerializeField] Rigidbody attachedRB;
+
+    Vector2 inputMovementVector;
+    Vector3 outputMovementVector;
+    Vector3 outputMovementVectorScaled;
+
+    public bool isSneaking, isMoving, isRunning;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    private void Awake()
     {
-        
+        currentPlayerSpeed = playerWalkSpeed;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnMovement(InputAction.CallbackContext ctx)
     {
-        
+        isMoving = true;
+        inputMovementVector = ctx.ReadValue<Vector2>();
+        if(ctx.canceled)
+            isMoving = false;
+    }
+
+    private void FixedUpdate()
+    {
+        if(isMoving)
+        { 
+            outputMovementVector = new Vector3(inputMovementVector.x, 0, inputMovementVector.y);
+            outputMovementVectorScaled = outputMovementVector * Time.deltaTime * currentPlayerSpeed;
+            transform.Translate(outputMovementVectorScaled); 
+        }
     }
 }
