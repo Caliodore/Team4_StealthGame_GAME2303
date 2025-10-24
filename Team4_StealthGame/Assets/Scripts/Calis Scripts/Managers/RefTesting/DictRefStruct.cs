a589c072d0998964b47a6d1c85f009b37dcda63b
Caliodore <caliodorecharbonneau@gmail.com>
2025-10-24 14:22:13 -0500

    Fleshing Out DictRefStruct
    Just trying to add a bit more to a test script for an organizational struct and attached class. Probably won't be implemented into game but helps me understand C# better.

--- Team4_StealthGame/Assets/Scripts/Calis Scripts/Managers/RefTesting/DictRefStruct.cs
+++ Team4_StealthGame/Assets/Scripts/Calis Scripts/Managers/RefTesting/DictRefStruct.cs
@@ -1,19 +1,69 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
 
+/// <summary>
+/// Just some testing at making a struct that cand hold a simple class for taking in an attached MonoBehaviour and determining how to group it.
+/// </summary>
 public class DictRefStruct : MonoBehaviour
 {
     /// <summary>
-    /// Concept for a single struct to put into dictionaries or arrays to tie to a singular gameObject so we can more easily compare references.
+    /// Concept for a struct to use when searching for an object in a universal dictionary.
+    /// Initialize it with a script reference attached to an object and assign an index number.
+    /// Then, using the script name reference in the dictionary, you can find a specific GameObjRef and any attached info without GetComponent.
     /// </summary>
     public struct DictRef
-    { 
-        private string name;
+    {
+        //Custom class for taking in MonoBehaviour and outputting name of script and the name of the object using it.
+        private readonly ObjectGroup<MonoBehaviour> groupType;
 
-        public string Name
+        private readonly string refGroup;
+        public string RefGroup { get { return refGroup; } }
+
+        private readonly int groupIndex;
+        public int GroupIndex { get { return groupIndex; } }
+
+        private readonly string objName;
+        public string ObjName { get { return objName; } }
+        
+        public DictRef(MonoBehaviour attachedScriptRef, int groupIndexInput)
         {
-            get { return name; }
-        }    
+            groupType = new ObjectGroup<MonoBehaviour>(attachedScriptRef);
+            refGroup = groupType.ObjScriptName;
+            groupIndex = groupIndexInput;
+            objName = groupType.ObjReference.name;
+        }
+    }
 
+    public class ObjectGroup<T> where T : MonoBehaviour
+    {
+        private T objType;
+        private string objScriptName;
+        private GameObject gameObjRef;
 
+        public ObjectGroup(T objTypeInput)
+        { 
+            objType = objTypeInput;
+            objScriptName = objType.name;
+            gameObjRef = objType.GetComponent<Transform>().gameObject;
+            if(gameObjRef == null)
+            { 
+                gameObjRef = objType.GetComponentInParent<Transform>().gameObject;
+                if(gameObjRef == null)
+                    print($"This Object Group is not attached to a GameObject.");
+            }
+        }
+
+        public string ObjScriptName
+        { 
+            get { return objScriptName; }    
+        }
+
+        public GameObject ObjReference
+        { 
+            get { return gameObjRef; }    
+        }
+        
     }
 }
