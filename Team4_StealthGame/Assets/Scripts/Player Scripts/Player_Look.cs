988b3ce136847095cee8b644238e8b5d8d919a2c
carlos-codes10 <charlitochiquito04@gmail.com>
2025-11-07 21:29:56 -0600

    Fixed Player Look
    merged commit conflicts probably caused this :D fixed it (again)

--- Team4_StealthGame/Assets/Scripts/Player Scripts/Player_Look.cs
+++ Team4_StealthGame/Assets/Scripts/Player Scripts/Player_Look.cs
@@ -15,20 +15,12 @@ public class Player_Look : MonoBehaviour
     // Center point Refrence
     [SerializeField] Transform centerPoint;
 
-    // look stats
-    [SerializeField] float lookSensitivityX = 1.0f;
-    [SerializeField] float lookSensitivityY = 1.0f; // Vertical sensitivity if needed (
-    public Vector3 cursorLocation;
-
     // private look variables
-    Vector2 lookInput;
-    float xRotation;
     private int floorMask;
     private float camRayLength = 100f;
     private Rigidbody playerRigidbody;
     Camera cam;
 
-    private AimingReticle attachedReticle;
 
 
     private void Start()
@@ -36,29 +28,17 @@ public class Player_Look : MonoBehaviour
         cam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
         floorMask = LayerMask.GetMask("Floor");
         playerRigidbody = GetComponent<Rigidbody>();
-        attachedReticle = gameObject.GetComponentInChildren<AimingReticle>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Look();
         Turning();
-        //cursorLocation = attachedReticle.GetCursorLocation();
-    }
-
-    void Look()
-    {
-        /*float lookX = lookInput.x * lookSensitivityX * Time.deltaTime;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
-
-        transform.Rotate(Vector3.up * lookX);*/
-        //centerPoint.LookAt(cursorLocation);
     }
 
     void Turning()
     {
-        /*Vector3 mousePos = Mouse.current.position.ReadValue(); // new input system
+        Vector3 mousePos = Mouse.current.position.ReadValue(); // new input system
 
         Ray camRay = Camera.main.ScreenPointToRay(mousePos);
         RaycastHit floorHit;
@@ -70,15 +50,6 @@ public class Player_Look : MonoBehaviour
 
             Quaternion newRotation = Quaternion.LookRotation(playerToMouse);
             playerRigidbody.MoveRotation(newRotation);
-        }*/
-
-        //transform.LookAt((attachedReticle.transform.forward * 2));
-    }
-
-
-
-    public void OnLook(InputValue v)
-    {
-        lookInput = v.Get<Vector2>();
+        }
     }
 }
